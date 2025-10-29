ab3bd743601080a6f26cb10ae5efe1550715d5d3
Sofia383048 <a383048@uach.mx>
2025-10-29 08:20:57 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ st.title("Ejemplo para usar session.state")
 if 'count' not in st.session_state:
   st.session_state['count']=0
 
+if "name" not in st.session_state:
+  st.session_state['name?]
+  
 if st.button('click me'):
   st.session_state['count']+=1
+
+nombre = st.text_input(#Escribe tu nombre")
+st.write(nombre)
   
 st.write(st.session_state)
