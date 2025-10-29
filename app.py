435ee21e2888358f6429ba2ebabe496e6f7c108a
Sofia383048 <a383048@uach.mx>
2025-10-29 08:05:48 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,5 @@ st.title("Ejemplo para usar session.state")
 if 'count' not in st.session_state:
   st.session_state['count']=0
 
-#count = 0
 
-increment = st.button("Increment")
-if increment:
-  count +=1
+st.write(st.session_state)
